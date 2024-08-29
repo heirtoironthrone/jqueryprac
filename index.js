5e243fea827edbdcf042cfd7e348b08b7819c323
heirtoironthrone <sushant7109xx@gmail.com>
2024-08-29 20:11:47 +0545

    added

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.btn1').click(function(){
+      $('.p1').hide()
+    })
+  })
+  
+  $(document).ready(function(){
+    $('.btn2').click(function(){
+      $('.p2').hide()
+    })
+  })
+  
